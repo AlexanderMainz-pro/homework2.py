ca4abeb4861a065b8f3f7979f7fc2e8d6ab1dd42
AlexanderKhlus <68031957+AlexanderMainz-pro@users.noreply.github.com>
2024-06-06 00:04:44 +0300

    homework2.py

---
+++ homework2.py
@@ -0,0 +1,7 @@
+completed_homework = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = number_of_hours_spent / completed_homework
+print('Курс: ', course_name,', ','Всего задач:', completed_homework,',',\
+      'Затраченно часов:',number_of_hours_spent,',', 'Среднее время выполнения',\
+      time_for_one_task, 'часа.' )
